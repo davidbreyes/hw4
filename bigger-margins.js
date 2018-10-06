7b2f774072cf56c6d3e49a4b6c01716fa931a0c5
davidbreyes <43257766+davidbreyes@users.noreply.github.com>
2018-10-06 00:02:03 -0700

    Update bigger-margins.js

--- bigger-margins.js
+++ bigger-margins.js
@@ -1,1 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+  for (var x = 150; x < width-200; x = x + 10) {
+    line(x, height/2, mouseX, mouseY);
+  }
+}
 
